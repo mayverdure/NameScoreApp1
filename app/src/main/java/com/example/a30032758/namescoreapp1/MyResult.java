571f798c5ba6f173f6e1fa3be6e639fc7ef68b13
Yamamoto <M.Yamamoto@jp.yokogawa.com>
2018-02-21 14:20:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a30032758/namescoreapp1/MyResult.java
@@ -0,0 +1,27 @@
+package com.example.a30032758.namescoreapp1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MyResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_result);
+
+        Intent intent = getIntent();
+        String myName = intent.getStringExtra(MyForm.EXTRA_MYNAME);
+        TextView nameLabel = (TextView) findViewById(R.id.nameLabel);
+        nameLabel.setText(myName + "の点数は...");
+
+        Random randomGenerator = new Random();
+        int score = randomGenerator.nextInt(101); // 0-100
+        TextView scoreLabel = (TextView) findViewById(R.id.scoreLabel);
+        scoreLabel.setText(String.valueOf(score) + "点!!");
+    }
+}
